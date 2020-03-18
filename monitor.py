27f20be73c59dc63dc1b678b39d944e97ddfa01c
Bryan Howard <bhowiebkr@gmail.com>
2020-03-17 23:38:48 -0400

    formatting

--- monitor.py
+++ monitor.py
@@ -15,12 +15,16 @@ palette = QtGui.QPalette()
 palette.setColor(QtGui.QPalette.Window, QtGui.QColor(27, 35, 38))
 palette.setColor(QtGui.QPalette.WindowText, QtGui.QColor(234, 234, 234))
 palette.setColor(QtGui.QPalette.Base, QtGui.QColor(27, 35, 38))
-palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.Base, QtGui.QColor(27 + 5, 35 + 5, 38 + 5))
+palette.setColor(
+    QtGui.QPalette.Disabled, QtGui.QPalette.Base, QtGui.QColor(27 + 5, 35 + 5, 38 + 5)
+)
 palette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(12, 15, 16))
 palette.setColor(QtGui.QPalette.ToolTipBase, QtGui.QColor(27, 35, 38))
 palette.setColor(QtGui.QPalette.ToolTipText, QtCore.Qt.white)
 palette.setColor(QtGui.QPalette.Text, QtGui.QColor(200, 200, 200))
-palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.Text, QtGui.QColor(100, 100, 100))
+palette.setColor(
+    QtGui.QPalette.Disabled, QtGui.QPalette.Text, QtGui.QColor(100, 100, 100)
+)
 palette.setColor(QtGui.QPalette.Button, QtGui.QColor(27, 35, 38))
 palette.setColor(QtGui.QPalette.ButtonText, QtCore.Qt.white)
 palette.setColor(QtGui.QPalette.BrightText, QtGui.QColor(100, 215, 222))
@@ -28,14 +32,15 @@ palette.setColor(QtGui.QPalette.Link, QtGui.QColor(126, 71, 130))
 palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(126, 71, 130))
 palette.setColor(QtGui.QPalette.HighlightedText, QtCore.Qt.white)
 palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.Light, QtCore.Qt.black)
-palette.setColor(QtGui.QPalette.Disabled, QtGui.QPalette.Shadow, QtGui.QColor(12, 15, 16))
+palette.setColor(
+    QtGui.QPalette.Disabled, QtGui.QPalette.Shadow, QtGui.QColor(12, 15, 16)
+)
 
 
 class NvidiaResources(QtWidgets.QWidget):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self)
-        self.setWindowTitle('Nvidia Resources')
-
+        self.setWindowTitle("Nvidia Resources")
 
         settings = QtCore.QSettings("nvidia-resources", "NvidiaResources")
 
@@ -49,7 +54,6 @@ class NvidiaResources(QtWidgets.QWidget):
         self.timer.start(FREQUENCY)
         self.index = MAX_DATAPOINTS
 
-
         self.chart = QtCharts.QChart()
         self.chart.setTheme(QtCharts.QChart.ChartThemeDark)
         self.series = QtCharts.QLineSeries(self)
@@ -57,19 +61,16 @@ class NvidiaResources(QtWidgets.QWidget):
 
         self.value_axis = QtCharts.QValueAxis()
         self.value_axis.setTickCount(5)
-        mem_total = nvmlDeviceGetMemoryInfo(handle).total/1024/1024
+        mem_total = nvmlDeviceGetMemoryInfo(handle).total / 1024 / 1024
         self.value_axis.setMax(mem_total)
-        self.series.attachAxis(self.value_axis)
         self.chart.addAxis(self.value_axis, QtCore.Qt.AlignLeft)
 
         for i in range(MAX_DATAPOINTS):
-            self.series.append(i,0)
+            self.series.append(i, 0)
 
         # Creating QChartView
         self.chart_view = QtCharts.QChartView(self.chart)
         self.chart_view.setRenderHint(QtGui.QPainter.Antialiasing)
-
-
 
         self.main_layout = QtWidgets.QHBoxLayout()
         self.main_layout.addWidget(self.chart_view)
@@ -78,14 +79,13 @@ class NvidiaResources(QtWidgets.QWidget):
 
     def Time(self):
         memInfo = nvmlDeviceGetMemoryInfo(handle)
-        used = memInfo.used/1048576
-        self.chart.setTitle(f"{used} MB") # 1024 / 1024
+        used = memInfo.used / 1048576
+        self.chart.setTitle(f"{used} MB")  # 1024 / 1024
         self.series.append(self.index, used)
         if self.index > MAX_DATAPOINTS:
             self.series.remove(0)
 
-        
-        self.index+=1
+        self.index += 1
 
         # I don't like this. must be a better way.
         self.chart.removeSeries(self.series)
@@ -95,11 +95,10 @@ class NvidiaResources(QtWidgets.QWidget):
     def closeEvent(self, event):
         self.settings = QtCore.QSettings("nvidia-resources", "NvidiaResources")
         self.settings.setValue("geometry", self.saveGeometry())
-        QtWidgets.QWidget.closeEvent(self, event   )
-  
+        QtWidgets.QWidget.closeEvent(self, event)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     app.setPalette(palette)
     widget = NvidiaResources()
